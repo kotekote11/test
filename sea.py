7c610a0142d50abf78aedb421a4ef8e5ac5cb754
kote <107511919+kotekote11@users.noreply.github.com>
2023-07-10 13:10:40 +0000

    Please enter the commit message for your changes. Lines starting

--- sea.py
+++ sea.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 
 # Функция для обработки команды /start
 def start_command(update: Update, context: CallbackContext) -> None:
