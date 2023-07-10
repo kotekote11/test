faad8475dc6558d84d073fe12f0cddf086d80bb8
kote <107511919+kotekote11@users.noreply.github.com>
2023-07-10 12:34:17 +0000

    Please enter the commit message for your changes. Lines starting

--- sea.py
+++ sea.py
@@ -1,28 +1,16 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, MessageFilters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Устанавливаем уровень логгирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-# Инициализируем Updater и передаем ему токен вашего бота
 updater = Updater(token='5700959339:AAEXSEfnjDg6zrl7bLUN1W_ISJtF6FiKd_0', use_context=True)
 dispatcher = updater.dispatcher
 
-# Обработчик команды /start
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот.")
+def handle_message(update, context):
+    text = update.message.text
+    if text == '/start':
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Hello!')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Received: ' + text)
 
-# Регистрируем обработчик команды /start
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+message_handler = MessageHandler(Filters.text, handle_message)
+dispatcher.add_handler(message_handler)
 
-# Обработчик текстовых сообщений
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-# Регистрируем обработчик текстовых сообщений
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
-# Запускаем бота
 updater.start_polling()
