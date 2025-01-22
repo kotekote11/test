98f79f3d0d729fb5739ff0151cecdfe924167fc9
kote <107511919+kotekote11@users.noreply.github.com>
2025-01-22 14:05:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import concurrent.futures
 import logging
 import subprocess
 import time
@@ -9,25 +8,15 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s]: %(message)s",
     handlers=[logging.StreamHandler()]
 )
+
 logger = logging.getLogger(__name__)
 
-def run_script(script_name):
-    logger.info(f"Запуск {script_name}...")
-    subprocess.run([f"python {script_name}"], capture_output=True)
-
 while True:
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        futures = {
-            executor.submit(run_script, "news_from_google.py"): "Google",
-            executor.submit(run_script, "news_from_yandex.py"): "Yandex"
-        }
-
-        for future in concurrent.futures.as_completed(futures):
-            service = futures[future]
-            try:
-                future.result()
-            except Exception as exc:
-                logger.exception(f"Ошибка при выполнении {service}: {exc}")
-
+    logger.info("Запуск news_from_google.py...")
+    subprocess.run(["python", "news_from_google.py"], capture_output=True)
+    
+    logger.info("Запуск news_from_yandex.py...")
+    subprocess.run(["python", "news_from_yandex.py"], capture_output=True)
+    
     logger.info("Ожидание перед следующим циклом...")
     time.sleep(1300)
